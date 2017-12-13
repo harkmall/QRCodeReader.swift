542b4f7b59d2f89e4ba2c21d77f0dfab2813b322
Mark Hall <mhall@konradgroup.com>
2017-12-13 11:28:09 -0500

    scanning from photos works now

--- Sources/QRCodeReaderViewController.swift
+++ Sources/QRCodeReaderViewController.swift
@@ -28,162 +28,192 @@ import UIKit
 import AVFoundation
 
 /// Convenient controller to display a view to scan/read 1D or 2D bar codes like the QRCodes. It is based on the `AVFoundation` framework from Apple. It aims to replace ZXing or ZBar for iOS 7 and over.
-public class QRCodeReaderViewController: UIViewController {
-  /// The code reader object used to scan the bar code.
-  public let codeReader: QRCodeReader
-
-  let readerView: QRCodeReaderContainer
-  let startScanningAtLoad: Bool
-  let showCancelButton: Bool
-  let showSwitchCameraButton: Bool
-  let showTorchButton: Bool
-  let showOverlayView: Bool
-
-  // MARK: - Managing the Callback Responders
-
-  /// The receiver's delegate that will be called when a result is found.
-  public weak var delegate: QRCodeReaderViewControllerDelegate?
-
-  /// The completion blocak that will be called when a result is found.
-  public var completionBlock: ((QRCodeReaderResult?) -> Void)?
-
-  deinit {
-    codeReader.stopScanning()
-
-    NotificationCenter.default.removeObserver(self)
-  }
-
-  // MARK: - Creating the View Controller
-
-  /**
-   Initializes a view controller using a builder.
-
-   - parameter builder: A QRCodeViewController builder object.
-   */
-  required public init(builder: QRCodeReaderViewControllerBuilder) {
-    readerView             = builder.readerView
-    startScanningAtLoad    = builder.startScanningAtLoad
-    codeReader             = builder.reader
-    showCancelButton       = builder.showCancelButton
-    showSwitchCameraButton = builder.showSwitchCameraButton
-    showTorchButton        = builder.showTorchButton
-    showOverlayView        = builder.showOverlayView
-
-    super.init(nibName: nil, bundle: nil)
-
-    view.backgroundColor = .black
-
-    codeReader.didFindCode = { [weak self] resultAsObject in
-      if let weakSelf = self {
-        if let qrv = weakSelf.readerView.displayable as? QRCodeReaderView {
-          qrv.addGreenBorder()
+public class QRCodeReaderViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    /// The code reader object used to scan the bar code.
+    public let codeReader: QRCodeReader
+    let imagePicker = UIImagePickerController()
+    
+    let readerView: QRCodeReaderContainer
+    let startScanningAtLoad: Bool
+    let showCancelButton: Bool
+    let showSwitchCameraButton: Bool
+    let showTorchButton: Bool
+    let showOverlayView: Bool
+    let showScanQRFromImageButton: Bool
+    
+    // MARK: - Managing the Callback Responders
+    
+    /// The receiver's delegate that will be called when a result is found.
+    public weak var delegate: QRCodeReaderViewControllerDelegate?
+    
+    /// The completion blocak that will be called when a result is found.
+    public var completionBlock: ((QRCodeReaderResult?) -> Void)?
+    
+    deinit {
+        codeReader.stopScanning()
+        
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // MARK: - Creating the View Controller
+    
+    /**
+     Initializes a view controller using a builder.
+     
+     - parameter builder: A QRCodeViewController builder object.
+     */
+    required public init(builder: QRCodeReaderViewControllerBuilder) {
+        readerView                = builder.readerView
+        startScanningAtLoad       = builder.startScanningAtLoad
+        codeReader                = builder.reader
+        showCancelButton          = builder.showCancelButton
+        showSwitchCameraButton    = builder.showSwitchCameraButton
+        showTorchButton           = builder.showTorchButton
+        showOverlayView           = builder.showOverlayView
+        showScanQRFromImageButton = builder.showScanQRFromImageButton
+        
+        super.init(nibName: nil, bundle: nil)
+        
+        view.backgroundColor = .black
+        
+        codeReader.didFindCode = { [weak self] resultAsObject in
+            if let weakSelf = self {
+                if let qrv = weakSelf.readerView.displayable as? QRCodeReaderView {
+                    qrv.addGreenBorder()
+                }
+                weakSelf.completionBlock?(resultAsObject)
+                weakSelf.delegate?.reader(weakSelf, didScanResult: resultAsObject)
+            }
         }
-        weakSelf.completionBlock?(resultAsObject)
-        weakSelf.delegate?.reader(weakSelf, didScanResult: resultAsObject)
-      }
-    }
-
-    codeReader.didFailDecoding = { [weak self] in
-      if let weakSelf = self {
-        if let qrv = weakSelf.readerView.displayable as? QRCodeReaderView {
-          qrv.addRedBorder()
+        
+        codeReader.didFailDecoding = { [weak self] in
+            if let weakSelf = self {
+                if let qrv = weakSelf.readerView.displayable as? QRCodeReaderView {
+                    qrv.addRedBorder()
+                }
+            }
         }
-      }
+        
+        setupUIComponentsWithCancelButtonTitle(builder.cancelButtonTitle, scanFromPhotosButtonTitle: builder.scanFromPhotosButtonTitle)
     }
-
-    setupUIComponentsWithCancelButtonTitle(builder.cancelButtonTitle)
-  }
-
-  required public init?(coder aDecoder: NSCoder) {
-    codeReader             = QRCodeReader()
-    readerView             = QRCodeReaderContainer(displayable: QRCodeReaderView())
-    startScanningAtLoad    = false
-    showCancelButton       = false
-    showTorchButton        = false
-    showSwitchCameraButton = false
-    showOverlayView        = false
-
-    super.init(coder: aDecoder)
-  }
-
-  // MARK: - Responding to View Events
-
-  override public func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-
-    if startScanningAtLoad {
-      startScanning()
+    
+    required public init?(coder aDecoder: NSCoder) {
+        codeReader                = QRCodeReader()
+        readerView                = QRCodeReaderContainer(displayable: QRCodeReaderView())
+        startScanningAtLoad       = false
+        showCancelButton          = false
+        showTorchButton           = false
+        showSwitchCameraButton    = false
+        showOverlayView           = false
+        showScanQRFromImageButton = false
+        
+        super.init(coder: aDecoder)
     }
-  }
-
-  override public func viewWillDisappear(_ animated: Bool) {
-    stopScanning()
-
-    super.viewWillDisappear(animated)
-  }
-
-  override public func viewWillLayoutSubviews() {
-    super.viewWillLayoutSubviews()
-
-    codeReader.previewLayer.frame = view.bounds
-  }
-
-  // MARK: - Initializing the AV Components
-
-  private func setupUIComponentsWithCancelButtonTitle(_ cancelButtonTitle: String) {
-    view.addSubview(readerView.view)
-
-    let sscb = showSwitchCameraButton && codeReader.hasFrontDevice
-    let stb  = showTorchButton && codeReader.isTorchAvailable
-
-    readerView.view.translatesAutoresizingMaskIntoConstraints = false
-    readerView.setupComponents(showCancelButton: showCancelButton, showSwitchCameraButton: sscb, showTorchButton: stb, showOverlayView: showOverlayView, reader: codeReader)
-
-    // Setup action methods
-
-    readerView.displayable.switchCameraButton?.addTarget(self, action: #selector(switchCameraAction), for: .touchUpInside)
-    readerView.displayable.toggleTorchButton?.addTarget(self, action: #selector(toggleTorchAction), for: .touchUpInside)
-    readerView.displayable.cancelButton?.setTitle(cancelButtonTitle, for: .normal)
-    readerView.displayable.cancelButton?.addTarget(self, action: #selector(cancelAction), for: .touchUpInside)
-
-    // Setup constraints
-
-    for attribute in [NSLayoutAttribute.left, NSLayoutAttribute.top, NSLayoutAttribute.right, NSLayoutAttribute.bottom] {
-      view.addConstraint(NSLayoutConstraint(item: readerView.view, attribute: attribute, relatedBy: .equal, toItem: view, attribute: attribute, multiplier: 1, constant: 0))
+    
+    // MARK: - Responding to View Events
+    
+    override public func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if startScanningAtLoad {
+            startScanning()
+        }
     }
-  }
-
-  // MARK: - Controlling the Reader
-
-  /// Starts scanning the codes.
-  public func startScanning() {
-    codeReader.startScanning()
-  }
-
-  /// Stops scanning the codes.
-  public func stopScanning() {
-    codeReader.stopScanning()
-  }
-
-  // MARK: - Catching Button Events
-
-  @objc func cancelAction(_ button: UIButton) {
-    codeReader.stopScanning()
-
-    if let _completionBlock = completionBlock {
-      _completionBlock(nil)
+    
+    override public func viewWillDisappear(_ animated: Bool) {
+        stopScanning()
+        
+        super.viewWillDisappear(animated)
     }
-
-    delegate?.readerDidCancel(self)
-  }
-
-  @objc func switchCameraAction(_ button: SwitchCameraButton) {
-    if let newDevice = codeReader.switchDeviceInput() {
-      delegate?.reader(self, didSwitchCamera: newDevice)
+    
+    override public func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        codeReader.previewLayer.frame = view.bounds
     }
-  }
-  
-  @objc func toggleTorchAction(_ button: ToggleTorchButton) {
-    codeReader.toggleTorch()
-  }
+    
+    // MARK: - Initializing the AV Components
+    
+    private func setupUIComponentsWithCancelButtonTitle(_ cancelButtonTitle: String, scanFromPhotosButtonTitle: String) {
+        view.addSubview(readerView.view)
+        
+        let sscb = showSwitchCameraButton && codeReader.hasFrontDevice
+        let stb  = showTorchButton && codeReader.isTorchAvailable
+        
+        readerView.view.translatesAutoresizingMaskIntoConstraints = false
+        readerView.setupComponents(showCancelButton: showCancelButton, showSwitchCameraButton: sscb, showTorchButton: stb, showScanFromPhotosButton: showScanQRFromImageButton,  showOverlayView: showOverlayView, reader: codeReader)
+        
+        // Setup action methods
+        
+        readerView.displayable.switchCameraButton?.addTarget(self, action: #selector(switchCameraAction), for: .touchUpInside)
+        readerView.displayable.toggleTorchButton?.addTarget(self, action: #selector(toggleTorchAction), for: .touchUpInside)
+        readerView.displayable.cancelButton?.setTitle(cancelButtonTitle, for: .normal)
+        readerView.displayable.cancelButton?.addTarget(self, action: #selector(cancelAction), for: .touchUpInside)
+        readerView.displayable.scanFromPhotosButton?.setTitle(scanFromPhotosButtonTitle, for: .normal)
+        readerView.displayable.scanFromPhotosButton?.addTarget(self, action: #selector(scanFromPhotosAction), for: .touchUpInside)
+        
+        // Setup constraints
+        
+        for attribute in [NSLayoutAttribute.left, NSLayoutAttribute.top, NSLayoutAttribute.right, NSLayoutAttribute.bottom] {
+            view.addConstraint(NSLayoutConstraint(item: readerView.view, attribute: attribute, relatedBy: .equal, toItem: view, attribute: attribute, multiplier: 1, constant: 0))
+        }
+    }
+    
+    // MARK: - Controlling the Reader
+    
+    /// Starts scanning the codes.
+    public func startScanning() {
+        codeReader.startScanning()
+    }
+    
+    /// Stops scanning the codes.
+    public func stopScanning() {
+        codeReader.stopScanning()
+    }
+    
+    // MARK: - Catching Button Events
+    
+    @objc func cancelAction(_ button: UIButton) {
+        codeReader.stopScanning()
+        
+        if let _completionBlock = completionBlock {
+            _completionBlock(nil)
+        }
+        
+        delegate?.readerDidCancel(self)
+    }
+    
+    @objc func switchCameraAction(_ button: SwitchCameraButton) {
+        if let newDevice = codeReader.switchDeviceInput() {
+            delegate?.reader(self, didSwitchCamera: newDevice)
+        }
+    }
+    
+    @objc func toggleTorchAction(_ button: ToggleTorchButton) {
+        codeReader.toggleTorch()
+    }
+    
+    @objc func scanFromPhotosAction(_ button: UIButton) {
+        stopScanning()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    //MARK: - UIImagePicker Delegate Methods
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            codeReader.scanQRCode(fromImage: pickedImage)
+        }
+        dismiss(animated: true, completion: nil)
+        startScanning()
+    }
+    
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+        startScanning()
+    }
+    
 }
--- Sources/QRCodeReaderViewControllerBuilder.swift
+++ Sources/QRCodeReaderViewControllerBuilder.swift
@@ -31,73 +31,83 @@ import Foundation
  the QRCode view controller.
  */
 public final class QRCodeReaderViewControllerBuilder {
-  // MARK: - Configuring the QRCodeViewController Objects
-
-  /**
-   The builder block.
-   The block gives a reference of builder you can configure.
-   */
-  public typealias QRCodeReaderViewControllerBuilderBlock = (QRCodeReaderViewControllerBuilder) -> Void
-
-  /**
-   The title to use for the cancel button.
-   */
-  public var cancelButtonTitle = "Cancel"
-
-  /**
-   The code reader object used to scan the bar code.
-   */
-  public var reader = QRCodeReader()
-
-  /**
-   The reader container view used to display the video capture and the UI components.
-   */
-  public var readerView = QRCodeReaderContainer(displayable: QRCodeReaderView())
-
-  /**
-   Flag to know whether the view controller start scanning the codes when the view will appear.
-   */
-  public var startScanningAtLoad = true
-
-  /**
-   Flag to display the cancel button.
-   */
-  public var showCancelButton = true
-
-  /**
-   Flag to display the switch camera button.
-   */
-  public var showSwitchCameraButton = true
-
-  /**
-   Flag to display the toggle torch button. If the value is true and there is no torch the button will not be displayed.
-   */
-  public var showTorchButton = false
-
-  /**
-   Flag to display the guide view.
-   */
-  public var showOverlayView = true
-
-  /**
-    Flag to display the guide view.
-  */
-  public var handleOrientationChange = true
-
-
-  // MARK: - Initializing a Flap View
-
-  /**
-   Initialize a QRCodeViewController builder with default values.
-   */
-  public init() {}
-
-  /**
-   Initialize a QRCodeReaderViewController builder with default values.
-
-   - parameter buildBlock: A QRCodeReaderViewController builder block to configure itself.
-   */
-  public init(buildBlock: QRCodeReaderViewControllerBuilderBlock) {
-    buildBlock(self)
-  }
+    // MARK: - Configuring the QRCodeViewController Objects
+    
+    /**
+     The builder block.
+     The block gives a reference of builder you can configure.
+     */
+    public typealias QRCodeReaderViewControllerBuilderBlock = (QRCodeReaderViewControllerBuilder) -> Void
+    
+    /**
+     The title to use for the cancel button.
+     */
+    public var cancelButtonTitle = "Cancel"
+    
+    /**
+     The title to use for the scan from photos button.
+     */
+    public var scanFromPhotosButtonTitle = "Scan from Photos"
+    
+    /**
+     The code reader object used to scan the bar code.
+     */
+    public var reader = QRCodeReader()
+    
+    /**
+     The reader container view used to display the video capture and the UI components.
+     */
+    public var readerView = QRCodeReaderContainer(displayable: QRCodeReaderView())
+    
+    /**
+     Flag to know whether the view controller start scanning the codes when the view will appear.
+     */
+    public var startScanningAtLoad = true
+    
+    /**
+     Flag to display the cancel button.
+     */
+    public var showCancelButton = true
+    
+    /**
+     Flag to display the switch camera button.
+     */
+    public var showSwitchCameraButton = true
+    
+    /**
+     Flag to display the toggle torch button. If the value is true and there is no torch the button will not be displayed.
+     */
+    public var showTorchButton = false
+    
+    /**
+     Flag to display the guide view.
+     */
+    public var showOverlayView = true
+    
+    /**
+     Flag to display the guide view.
+     */
+    public var handleOrientationChange = true
+    
+    /**
+     Flag to display the scan from an image button.
+     */
+    public var showScanQRFromImageButton = false
+    
+    
+    // MARK: - Initializing a Flap View
+    
+    /**
+     Initialize a QRCodeViewController builder with default values.
+     */
+    public init() {}
+    
+    /**
+     Initialize a QRCodeReaderViewController builder with default values.
+     
+     - parameter buildBlock: A QRCodeReaderViewController builder block to configure itself.
+     */
+    public init(buildBlock: QRCodeReaderViewControllerBuilderBlock) {
+        buildBlock(self)
+    }
 }
